b6ec7f527c1930cc595d26d6631ba5a0db1be5b2
Ali Kaafarani <ali@kvikshaug.no>
2016-11-02 09:40:21 +0100

    feat: add Focus connection attempt

--- src/focus.py
+++ src/focus.py
@@ -1,1 +1,44 @@
+import multiprocessing
+import os
+import pyodbc
+import signal
+
 from secrets import secrets
+
+def main():
+    host, port = secrets['DATABASES_FOCUS_HOST_TEST'].split(',')
+    connection_string = 'DRIVER={Freetds};SERVER=%s;PORT=%s;DATABASE=%s;UID=%s;PWD=%s' % (
+        host,
+        port,
+        secrets['DATABASES_FOCUS_NAME_PROD'],
+        secrets['DATABASES_FOCUS_USER_TEST'],
+        secrets['DATABASES_FOCUS_PASSWORD_TEST'],
+    )
+
+    timeout = 5
+
+    def attempt_connection():
+        connection = pyodbc.connect(connection_string, timeout=timeout)
+        cursor = connection.cursor()
+        cursor.execute('select @@version').fetchall()
+        cursor.close()
+        connection.close()
+
+    # Since the connection may hang indefinitely (see comment on 'connection_timeout' in Focus DB definition)
+    # perform the connection in a separate process. pyodbc acquires the GIL lock during connection, so signalling
+    # or threading wouldn't work here.
+    connection_process = multiprocessing.Process(target=attempt_connection)
+    connection_process.start()
+    connection_process.join(timeout)
+
+    # If the connection attempt didn't finish, terminate it; it will get a non-zero exit code
+    if connection_process.is_alive():
+        # connection_process.terminate() sends SIGINT and pyodbc doesn't seem to respond to that while blocking. It
+        # does respond to SIGHUP, so send that.
+        os.kill(connection_process.pid, signal.SIGHUP)
+        connection_process.join()
+
+    focus_available = connection_process.exitcode == 0
+
+if __name__ == '__main__':
+    main()
