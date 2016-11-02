b96860f037806cb71722eed0a7080cd999db499d
Ali Kaafarani <ali@kvikshaug.no>
2016-11-02 15:45:08 +0100

    feat: implement infinite loop

--- src/focus.py
+++ src/focus.py
@@ -4,6 +4,7 @@ import pylibmc
 import pyodbc
 from raven import Client
 import signal
+import time
 
 from secrets import secrets
 
@@ -14,53 +15,61 @@ else:
 
 
 def main():
-    host, port = secrets['DATABASES_FOCUS_HOST_PROD'].split(',')
-    connection_string = ';'.join([
-        'DRIVER={FreeTDS}',
-        'SERVER=%s' % host,
-        'PORT=%s' % port,
-        'DATABASE=%s' % secrets['DATABASES_FOCUS_NAME_PROD'],
-        'UID=%s' % secrets['DATABASES_FOCUS_USER_PROD'],
-        'PWD=%s' % secrets['DATABASES_FOCUS_PASSWORD_PROD'],
-    ])
+    # How frequent to attempt connection in seconds. Should be low enough to
+    # discover a non-working connection as soon as possible, but high enough not
+    # to cause significant load on the database.
+    check_interval = 1
 
-    timeout = 5
-    cache_time = 10
+    while True:
+        host, port = secrets['DATABASES_FOCUS_HOST_PROD'].split(',')
+        connection_string = ';'.join([
+            'DRIVER={FreeTDS}',
+            'SERVER=%s' % host,
+            'PORT=%s' % port,
+            'DATABASE=%s' % secrets['DATABASES_FOCUS_NAME_PROD'],
+            'UID=%s' % secrets['DATABASES_FOCUS_USER_PROD'],
+            'PWD=%s' % secrets['DATABASES_FOCUS_PASSWORD_PROD'],
+        ])
 
-    def attempt_connection():
-        connection = pyodbc.connect(connection_string, timeout=timeout)
-        cursor = connection.cursor()
-        cursor.execute('select @@version').fetchall()
-        cursor.close()
-        connection.close()
+        timeout = 5
+        cache_time = 10
 
-    # Note that `timeout` argument will *not* ensure consistent timeouts for any
-    # connection problem. It sets the ODBC API connection attribute
-    # SQL_ATTR_LOGIN_TIMEOUT, but not SQL_ATTR_CONNECTION_TIMEOUT.
-    # See https://github.com/mkleehammer/pyodbc/issues/106
-    # Therefore, perform the connection in a separate process. pyodbc acquires
-    # the GIL lock during connection, so signalling or threading wouldn't work
-    # here.
-    connection_process = multiprocessing.Process(target=attempt_connection)
-    connection_process.start()
-    connection_process.join(timeout)
+        def attempt_connection():
+            connection = pyodbc.connect(connection_string, timeout=timeout)
+            cursor = connection.cursor()
+            cursor.execute('select @@version').fetchall()
+            cursor.close()
+            connection.close()
 
-    # If the connection attempt didn't finish, terminate it; it will get a
-    # non-zero exit code
-    if connection_process.is_alive():
-        # connection_process.terminate() sends SIGINT and pyodbc doesn't seem to
-        # respond to that while blocking. It
-        # does respond to SIGHUP, so send that.
-        os.kill(connection_process.pid, signal.SIGHUP)
-        connection_process.join()
+        # Note that `timeout` argument will *not* ensure consistent timeouts for
+        # any connection problem. It sets the ODBC API connection attribute
+        # SQL_ATTR_LOGIN_TIMEOUT, but not SQL_ATTR_CONNECTION_TIMEOUT.
+        # See https://github.com/mkleehammer/pyodbc/issues/106
+        # Therefore, perform the connection in a separate process. pyodbc
+        # acquires the GIL lock during connection, so signalling or threading
+        # wouldn't work here.
+        connection_process = multiprocessing.Process(target=attempt_connection)
+        connection_process.start()
+        connection_process.join(timeout)
 
-    focus_available = connection_process.exitcode == 0
-    mc = pylibmc.Client(
-        ["memcached"],
-        binary=True,
-        behaviors={"tcp_nodelay": True, "ketama": True},
-    )
-    mc.set("focus.connection", focus_available, time=cache_time)
+        # If the connection attempt didn't finish, terminate it; it will get a
+        # non-zero exit code
+        if connection_process.is_alive():
+            # connection_process.terminate() sends SIGINT and pyodbc doesn't
+            # seem to respond to that while blocking. It does respond to SIGHUP,
+            # so send that.
+            os.kill(connection_process.pid, signal.SIGHUP)
+            connection_process.join()
+
+        focus_available = connection_process.exitcode == 0
+        mc = pylibmc.Client(
+            ["memcached"],
+            binary=True,
+            behaviors={"tcp_nodelay": True, "ketama": True},
+        )
+        mc.set("focus.connection", focus_available, time=cache_time)
+
+        time.sleep(check_interval)
 
 if __name__ == '__main__':
     try:
