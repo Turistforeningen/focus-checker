59b7befd4c37b9dc0ba0a846a18286818254aa1e
Ali Kaafarani <ali@kvikshaug.no>
2016-11-02 14:03:34 +0100

    chore: create separate secrets volume for project

--- src/secrets.py
+++ src/secrets.py
@@ -1,11 +1,4 @@
-secrets = {}
+import json
 
-# Read secrets from mountpoint in envs where the mount is expected
-with open('/secrets/settings.env') as secrets_file:
-    for line in secrets_file:
-        # Simple env file parsing
-        line = line.strip()
-        if line.startswith('#') or line == '':
-            continue
-        key, value = line.split('=', 1)
-        secrets[key] = value
+with open('/secrets/secrets.json') as f:
+    secrets = json.loads(f.read())
