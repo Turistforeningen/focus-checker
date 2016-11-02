c7926085bcb479802a11c5b11a883f371d22872e
Ali Kaafarani <ali@kvikshaug.no>
2016-11-02 08:50:39 +0100

    feat: read secrets

---
+++ src/secrets.py
@@ -0,0 +1,13 @@
+import os
+
+secrets = {}
+
+# Read secrets from mountpoint in envs where the mount is expected
+with open('/secrets/settings.env') as secrets_file:
+    for line in secrets_file:
+        # Simple env file parsing
+        line = line.strip()
+        if line.startswith('#') or line == '':
+            continue
+        key, value = line.split('=', 1)
+        secrets[key] = value
